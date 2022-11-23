8ad04f90f0b8334411e691ea05a977533f2f4575
Shivam Sharma <shivam@Shivams-MacBook-Air.local>
2022-11-24 01:20:51 +0530

    initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,106 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+import { MapOutlined, SignalCellularNoSimOutlined, SourceOutlined, TrackChangesOutlined, WifiCalling3Rounded } from "@mui/icons-material";
+
+const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">AquaTech</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">MANAGE</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Acquifers</span>
+            </li>
+          </Link>
+          <li>
+            <SourceOutlined className="icon" />
+            <span>About</span>
+          </li>
+
+          <p className="title">SERVICE</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Statistics</span>
+          </li>
+          <li>
+            <TrackChangesOutlined className="icon" />
+            <span>Simulation</span>
+          </li>
+          <li>
+            <MapOutlined className="icon" />
+            <span>Global Maps</span>
+          </li>
+
+          <p className="title">OPERATIONS</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>Acquifer Reports</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/know-your-aquifer/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./search.css";
+import Map from "../../components/map";
+import RectangleContainer from "../../components/rectangle-container";
+import { state_arr, s_a } from "../../utils/city";
+import Select from "react-select";
+
+let stateList = state_arr.map((el) => {
+  return { value: el, label: el };
+});
+
+function KnowyourAquifer() {
+  return (
+    <div>
+      <div className="a-flex">
+        <div className="ml-16">
+          <RectangleContainer>
+            <div className="a-flex a-fdc a-jcsb a-hfull p8">
+              <div className="state-text">Select State</div>
+              <Select options={stateList} />
+            </div>
+          </RectangleContainer>
+        </div>
+        <div className="ml-16 a-hfull p8">
+          <RectangleContainer >Hi</RectangleContainer>
+        </div>
+        <div className="ml-16 a-hfull p8">
+          <RectangleContainer >Hi</RectangleContainer>
+        </div>
+      </div>
+      <div className="map-container">
+        <Map />
+      </div>
+    </div>
+  );
+}
+
+export default KnowyourAquifer;
